7e2693e70daebb3877b5702704efa26c668923c9
ObedRav <obedrayo2356@gmail.com>
2023-04-11 22:18:27 +0000

    Adding the status

---
+++ api/v1/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+
+# Register the app_views blueprint
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(ctx):
+    """If the program finish"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'), port=getenv('HBNB_API_PORT', 5000), threaded=True)
