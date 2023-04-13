8a1522cb7e098116b5e7a43700876b6e00606c8a
ObedRav <obedrayo2356@gmail.com>
2023-04-13 20:37:57 +0000

    Adding comments

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """Package that manage all the routes"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1') # Adding a url prefix
 
 from api.v1.views.index import *
 from api.v1.views.states import *
